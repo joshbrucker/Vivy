ddae53a4cb3fe70676c2f48682fe6a6a6039155f
Joshua Brucker <joshbrucker@gmail.com>
2022-01-29 12:47:39 -0500

    Register slash commands globally.

--- src/event_handlers/client_ready/client-ready.js
+++ src/event_handlers/client_ready/client-ready.js
@@ -5,14 +5,14 @@ const Commands = require(__basedir + "/commands/Commands.js");
 const auth = require(__basedir + "/auth.json");
 
 function onClientReady(client) {
-  const rest = new REST({ version: '9' }).setToken(auth.token);
+  const rest = new REST({ version: "9" }).setToken(auth.token);
   const commandJSONs = Commands.map(command => command.data);
 
-  rest.put(Routes.applicationGuildCommands(auth.clientId, auth.testGuild), { body: commandJSONs })
-      .then(() => console.log('Successfully registered application commands.'))
+  rest.put(Routes.applicationCommands(client.user.id), { body: commandJSONs })
+      .then(() => console.log("Successfully registered application commands."))
       .catch(console.error);
 
-  console.log('I am ready!');
+  console.log("I'm ready to perform~!");
 }
 
 module.exports = onClientReady;
