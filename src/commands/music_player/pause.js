7e5e0d6db1da652a037fb57fc14c3dc8a278ff3b
Josh Brucker <joshbrucker@gmail.com>
2022-06-05 16:58:39 -0400

    Add pause and resume commands.

---
+++ src/commands/music_player/pause.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName("pause")
+      .setDescription("Pauses the music player."),
+
+  async execute(interaction) {
+    if (!interaction.member.voice.channel) {
+      await interaction.reply("You must be in voice to use this command!");
+      return;
+    }
+
+    let player = interaction.client.player;
+    let guild = interaction.guild;
+    let queue = player.getQueue(guild.id);
+
+    if (!queue || !queue.songs || queue.songs.length === 0) {
+      await interaction.reply("There is nothing playing!");
+      return;
+    }
+
+    if (queue.paused) {
+      await interaction.reply("The music player is already paused!");
+      return;
+    }
+
+    await queue.setPaused(true);
+    await interaction.reply("Paused the music player!");
+  }
+};
---
+++ src/commands/music_player/resume.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName("resume")
+      .setDescription("Resumes a paused music player."),
+
+  async execute(interaction) {
+    if (!interaction.member.voice.channel) {
+      await interaction.reply("You must be in voice to use this command!");
+      return;
+    }
+
+    let player = interaction.client.player;
+    let guild = interaction.guild;
+    let queue = player.getQueue(guild.id);
+
+    if (!queue || !queue.songs || queue.songs.length === 0) {
+      await interaction.reply("There is nothing playing!");
+      return;
+    }
+
+    if (!queue.paused) {
+      await interaction.reply("The music player is already resumed!");
+      return;
+    }
+
+    await queue.setPaused(false);
+    await interaction.reply("Resumed the music player!");
+  }
+};
