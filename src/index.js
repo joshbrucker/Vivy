3d98affefb5903e6e01d5eec9e15ddaeccb98863
Josh Brucker <joshbrucker@gmail.com>
2022-06-26 12:45:42 -0400

    Add top.gg integration.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+
+const { AutoPoster } = require("topgg-autoposter");
 const { ShardingManager } = require("discord.js");
 const auth = require("./auth.json");
 
 const manager = new ShardingManager("./bot.js", { token: auth.token });
+const poster = AutoPoster(auth.topggToken, manager);
 
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
