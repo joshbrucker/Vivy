bcafc4c8112274429bf6799de1569c9f18219c85
Josh Brucker <joshbrucker@gmail.com>
2022-09-29 17:43:59 -0400

    Handle top.gg poster errors.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const { ShardingManager } = require("discord.js");
 const auth = require("./auth.json");
 
 const manager = new ShardingManager("./bot.js", { token: auth.token });
+
 const poster = AutoPoster(auth.topggToken, manager);
+poster.on("error", (_) => {
+  console.log("Issue reaching top.gg website. Will try again later...");
+});
 
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
