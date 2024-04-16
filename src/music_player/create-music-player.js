c0356e5fb0e7e4b7c7207fb8ca3858fc354b06d5
Josh Brucker <joshbrucker@gmail.com>
2024-04-16 01:02:39 -0400

    Add youtube identity token header

--- src/music_player/create-music-player.js
+++ src/music_player/create-music-player.js
@@ -9,7 +9,8 @@ module.exports = async (client) => {
     ytdlOptions: {
       requestOptions: {
         headers: {
-          cookie: auth["youtubeCookie"],
+          cookie: auth["youtubeCookies"],
+          "x-youtube-identity-token": auth["youtubeIdToken"]
         }
       }
     }
