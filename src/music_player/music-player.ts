99d836cf8369b4d6536aeea7d29f0d95033151fd
Joshua Brucker <joshbrucker@gmail.com>
2026-08-30 13:15:37 -0400

    Remove explicit proxy and ffmpeg-static

--- src/music_player/music-player.ts
+++ src/music_player/music-player.ts
@@ -1,10 +1,8 @@
 // import { DefaultExtractors } from "@discord-player/extractor";
 import auth from "../auth.json";
-import settings from "../settings.json";
 import { Player } from "discord-player";
 import { YoutubeExtractor, YoutubeOptions } from "discord-player-youtubei";
 import { Client } from "discord.js";
-import { ProxyAgent } from "undici";
 
 export class MusicPlayer extends Player {
   constructor(client: Client) {
@@ -20,10 +18,6 @@ export class MusicPlayer extends Player {
         trialOrder: ["sabr"],
       },
     };
-
-    for (const url of settings.proxyAddress) {
-      youtubeiSettings.proxy = new ProxyAgent({ uri: url });
-    }
 
     await this.extractors.register(YoutubeExtractor, youtubeiSettings);
   }
